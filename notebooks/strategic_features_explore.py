ddccbcf8ac006326384731ed8c4ff819c92495a8
Chaitanya Mangla <cmangla@users.noreply.github.com>
2020-11-22 17:52:53 +0000

    More problem examples in notebook

--- notebooks/strategic_features_explore.py
+++ notebooks/strategic_features_explore.py
@@ -13,7 +13,9 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 #%%
+f = get_problem_features(tptp.problems['PUZ069+2'])
 
+#%%
 f = get_problem_features(tptp.problems['SET995+1'])
 
 # %%
@@ -27,6 +29,12 @@ f = get_problem_features(tptp.problems['KRS180+1'])
 # %%
 # This has formula selection
 f = get_problem_features(tptp.problems['SYN000+2'])
+
+# %%
+# Problems/LAT/LAT300+2.p
+# Includes cannot be found
+a = tptp.find_by_filename('Axioms/SET007/SET007+0.ax')
+f = get_problem_features(tptp.problems['LAT300+2'])
 
 #%%
 import random
