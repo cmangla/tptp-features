4f912022bff8b91c40fff34ab85811f286192b96
Chaitanya Mangla <cmangla@users.noreply.github.com>
2020-11-23 17:14:31 +0000

    Use newly implemented features

--- notebooks/save_extracted_features.py
+++ notebooks/save_extracted_features.py
@@ -7,26 +7,25 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 #%%
-SINGLE_TIMEOUT = 20
-TOTAL_TIMEOUT = 60
+SINGLE_TIMEOUT = 10
+TOTAL_TIMEOUT = 20
 
 #%%
 from tptp_features.Tptp import Tptp
 import random
 random.seed()
-tptp = Tptp("../tptp-parser/")
+import os
+os.environ['TPTP'] = '/Users/cm772/Documents/Dev/tptp-parser'
+tptp = Tptp('/Users/cm772/Documents/Dev/tptp-parser')
 problems = list(tptp.get_problems({'SPC': 'FOF_.*'}))
+
+#%%
 random.shuffle(problems)
-#problems = problems[:30]
+problems = problems[:30]
 
 #%%
 from tptp_features.strategic_features_rq_pure import get_features
-data, incomplete = get_features(problems, SINGLE_TIMEOUT, TOTAL_TIMEOUT)
-
-#%%
-import pandas as pd
-incomplete = pd.Series(list(incomplete))
-incomplete.describe()
+data, failed = get_features(problems, SINGLE_TIMEOUT, TOTAL_TIMEOUT)
 
 #%%
 store = pd.HDFStore('problem_features.h5')
